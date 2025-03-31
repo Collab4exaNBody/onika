3fa5595e07b766d2033ae6123cd7727d17d11a1a
Thierry Carrard <thierry.carrard@cea.fr>
2025-03-31 18:30:51 +0200

    separate headers

--- include/onika/parallel/parallel_execution_context.h
+++ include/onika/parallel/parallel_execution_context.h
@@ -25,10 +25,12 @@ under the License.
 #include <onika/memory/allocator.h>
 
 #include <onika/parallel/constants.h>
+#include <onika/parallel/parallel_execution_space.h>
+#include <onika/parallel/parallel_data_access.h>
+#include <onika/parallel/constants.h>
+#include <onika/flat_tuple.h>
 
 #include <mutex>
-#include <condition_variable>
-#include <span>
 
 namespace onika
 {
@@ -68,29 +70,6 @@ namespace onika
     {
       void(*m_func)(ParallelExecutionContext*,void*) = nullptr;
       void *m_data = nullptr;
-    };
-
-    template<unsigned int ND=1> struct ElementCoordT { using type = onika::oarray_t<ssize_t,ND>; };
-    template<> struct ElementCoordT<1> { using type = ssize_t; };
-    template<unsigned int ND> using element_coord_t = typename ElementCoordT<ND>::type;
-    
-    template<class T, bool = std::is_integral_v<T> > struct ElementCoordND { static inline constexpr unsigned int value = 1; };
-    template<class T> struct ElementCoordND<T,false> { static inline constexpr unsigned int value = T::array_size; };
-    template<class T> static inline constexpr unsigned int element_coord_nd_v = ElementCoordND<T>::value;
-
-    template<unsigned int _NDim=1, unsigned int _ElementListNDim=0, class _ElementListT = std::span< const element_coord_t<_ElementListNDim> > >
-    struct ParallelExecutionSpace
-    {
-      static_assert( _NDim>=1 && _NDim<=3 && _ElementListNDim>=0 && _ElementListNDim<=3 );
-      static_assert( _ElementListNDim==0 || _NDim==1 , "Element lists are only supported for 1D parallel execution spaces" );
-      static inline constexpr unsigned int NDim = _NDim;
-      static inline constexpr unsigned int ElementListNDim = _ElementListNDim;
-      using coord_t = onika::oarray_t<ssize_t,NDim>;
-      using element_list_t = _ElementListT;
-      using element_t = std::remove_cv_t< std::remove_reference_t< decltype( _ElementListT{}[0] ) > >;
-      coord_t m_start;
-      coord_t m_end;
-      element_list_t m_elements = {};
     };
 
     struct ParallelExecutionQueue;
