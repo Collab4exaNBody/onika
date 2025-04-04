efe54e3e1f4cb11a6abec949114e9674e3e5ff01
Thierry Carrard <thierry.carrard@cea.fr>
2025-04-04 09:10:44 +0200

    preparinf PDA integration into kernel execution workflow

--- include/onika/parallel/parallel_data_access.h
+++ include/onika/parallel/parallel_data_access.h
@@ -22,6 +22,7 @@ under the License.
 #include <onika/oarray.h>
 #include <onika/flat_tuple.h>
 #include <type_traits>
+#include <cmath>
 
 namespace onika
 {
@@ -29,31 +30,98 @@ namespace onika
   namespace parallel
   {
 
-    struct ParallelDataAccess
+    namespace ParallelDataAccessConstants
     {
       static inline constexpr int8_t RO = 0x01;
       static inline constexpr int8_t WO = 0x02;
       static inline constexpr int8_t RW = 0x03;
       static inline constexpr int8_t END = 0xFF;
+    }
 
-      const void * const m_data_ptr = nullptr; // acts as a unique id for data. can be object's address, array start, etc.
-      const unsigned int m_ndim = 0; // 0 means scalar, or, not parallel execution space correlated data access
-      const unsigned int m_stencil_size = 0;
-      const int8_t m_access_stencil[4*28] ; // number of elements mus be (m_ndim+1) * m_stencil_size
-      const char m_name[16] = {'\0',}; // optional, may hold a string constant with the name of accessed data
+    struct ParallelDataAccess
+    {      
+      static inline constexpr size_t MAX_NAME_LENGTH = 16;
+
+      const void * m_data_ptr = nullptr; // acts as a unique id for data. can be object's address, array start, etc.
+      unsigned int m_ndim = 0; // 0 means scalar, or, not parallel execution space correlated data access
+      unsigned int m_stencil_size = 0;
+      int8_t m_access_stencil[4*28] = { ParallelDataAccessConstants::END, }; // number of elements mus be (m_ndim+1) * m_stencil_size
+      char m_name[MAX_NAME_LENGTH] = {'\0',}; // optional, may hold a string constant with the name of accessed data
       
-      void set_ndim(unsigned int nd);
-      void add_stencil_access( int8_t mode, int8_t ri, int8_t rj=0, int8_t rk=0 );
-      void set_name(const std::string& s);
-      void set_address(const void * p);
-      
-      const char* name() const;
+      inline void set_ndim(unsigned int nd) { assert( nd <= 3 ); m_ndim = nd; }
+      inline unsigned int ndim() const { return m_ndim; }
+      inline void reset() { m_stencil_size=0; m_access_stencil[0]=ParallelDataAccessConstants::END; }
+      inline void set_name(const std::string& s) { strncpy( m_name , s.c_str() , MAX_NAME_LENGTH ); m_name[MAX_NAME_LENGTH-1]='\0'; }
+      inline void set_address(const void * p) { asser( p != nullptr ); m_data_ptr = p; }
+      inline const char* name() const { return m_name; }
+      inline void add_stencil_access( int8_t mode, int8_t ri, int8_t rj=0, int8_t rk=0 )
+      {
+        using namespace ParallelDataAccessConstants;
+        m_access_stencil[ m_stencil_size*(ndim()+1) + 0 ] = mode;
+        m_access_stencil[ m_stencil_size*(ndim()+1) + 1 ] = ri;
+        if( ndim() >= 2 ) m_access_stencil[ m_stencil_size*(ndim()+1)+2] = rj;
+        if( ndim() >= 3 ) m_access_stencil[ m_stencil_size*(ndim()+1)+3] = rk;
+        ++ m_stencil_size;
+        m_access_stencil[ m_stencil_size*(ndim()+1) ] = END;
+      }
     };
 
-    /*
-      // exemple usage. note that stencil mus tbe static and const, it's lifespan must be longer than it's usage for any parallel kernel.
-      ParallelDataAccess pda = { my_array_2d , 2 , 4 , { RW,0,0 , RO,-1,0 , RO,1,0 , RO,0,-1 , RO,0,1 , END } , "velocity" };
-    */
+
+
+    // exemple usage. note that stencil mus tbe static and const, it's lifespan must be longer than it's usage for any parallel kernel.
+    inline ParallelDataAccess access_cross_2d(const void * p , int8_t center_mode = ParallelDataAccessConstants::RW, int8_t side_mode = ParallelDataAccessConstants::RO , const char* name_tag = nullptr)
+    {
+      using namespace ParallelDataAccessConstants;
+      ParallelDataAccess pda = { p , 2 , 5 ,
+        { center_mode, 0, 0 
+        , side_mode  ,-1, 0 , side_mode,1,0 
+        , side_mode  , 0,-1 , side_mode,0,1 
+        , END } , {'*','\0',} };
+      if( name_tag != nullptr ) pda.set_name( name_tag );
+      return pda;
+    }
+
+    inline ParallelDataAccess access_cross_3d(const void * p , int8_t center_mode = ParallelDataAccessConstants::RW, int8_t side_mode = ParallelDataAccessConstants::RO , const char* name_tag = nullptr)
+    {
+      using namespace ParallelDataAccessConstants;
+      ParallelDataAccess pda = { p , 3 , 7 ,
+      { center_mode, 0, 0, 0 
+      , side_mode  ,-1, 0, 0 , side_mode,1,0,0 
+      , side_mode  , 0,-1, 0 , side_mode,0,1,0 
+      , side_mode  , 0, 0,-1 , side_mode,0,0,1
+      , END } , {'*','\0',} };
+      if( name_tag != nullptr ) pda.set_name( name_tag );
+      return pda;
+    }
+
+    inline ParallelDataAccess access_nbh_3d(const void * p , int8_t center_mode = ParallelDataAccessConstants::RW, int8_t side_mode = ParallelDataAccessConstants::RO , const char* name_tag = nullptr)
+    {
+      using namespace ParallelDataAccessConstants;
+      ParallelDataAccess pda = { p , 3 , 27 , { END, } , {'*','\0',} };
+      int idx = 0;
+      for(int k=-1;k<=1;k++)
+      for(int j=-1;j<=1;j++)
+      for(int i=-1;i<=1;i++)
+      {
+        pda.m_access_stencil[ idx ++ ] = (std::abs(i)+std::abs(j)+std::abs(k))==0 ? center_mode : side_mode;
+        pda.m_access_stencil[ idx ++ ] = i;
+        pda.m_access_stencil[ idx ++ ] = j;
+        pda.m_access_stencil[ idx ++ ] = k;
+      }
+      pda.m_access_stencil[ idx ++ ] = END;
+      assert( idx == (3+1)*27 + 1 );
+      if( name_tag != nullptr ) pda.set_name( name_tag );
+      return pda;
+    }
+
+    inline ParallelDataAccess local_access(const void * p , unsigned int nd=3, int8_t mode = ParallelDataAccessConstants::RW , const char* name_tag = nullptr)
+    {
+      using namespace ParallelDataAccessConstants;
+      ParallelDataAccess pda = { p , nd , 1 , { mode,0,0,0, } , {'*','\0',} };
+      pda.m_access_stencil [ nd + 1 ] = END;
+      if( name_tag != nullptr ) pda.set_name( name_tag );
+      return pda;
+    }
 
   }
 
--- include/onika/parallel/parallel_execution_operators.h
+++ include/onika/parallel/parallel_execution_operators.h
@@ -6,6 +6,7 @@
 #include <onika/parallel/stream_utils.h>
 #include <onika/parallel/parallel_execution_stream.h>
 #include <onika/parallel/parallel_execution_queue.h>
+#include <onika/parallel/parallel_data_access.h>
 #include <mutex>
 #include <atomic>
 
@@ -49,12 +50,14 @@ namespace onika
       auto * pec = pew.m_pec;
       pew.m_pec = nullptr;
       pesq.enqueue( pec );
+      pesq.reset_data_access();
       return pesq;
     }
 
     inline ParallelExecutionQueue& operator << ( ParallelExecutionQueue& pesq , flush_t )
     {
       pesq.schedule_all();
+      pesq.set_lane(DEFAULT_EXECUTION_LANE);
       return pesq;
     }
 
@@ -70,6 +73,18 @@ namespace onika
       return pesq;
     }
 
+    inline ParallelExecutionQueue& operator << ( ParallelExecutionQueue& pesq , const ParallelDataAccess& pda )
+    {
+      pesq.add_data_access( pda );
+      return pesq;
+    }
+    
+    inline ParallelExecutionQueue& operator << ( ParallelExecutionQueue& pesq , ParallelDataAccess && pda )
+    {
+      pesq.add_data_access( pda );
+      return pesq;
+    }
+
     inline ParallelExecutionWrapper::~ParallelExecutionWrapper()
     {
       if( m_pec != nullptr )
@@ -79,6 +94,13 @@ namespace onika
           fatal_error() << "No default queue to schedule parallel operation" << std::endl;
         }
         auto * q = m_pec->m_default_queue;
+
+        // synchronize and reset queue
+        q->wait();
+        q->reset_data_access();
+        q->set_lane( DEFAULT_EXECUTION_LANE );
+        
+        // immediate scheduling and execution of parallel task
         q->enqueue( m_pec );
         m_pec = nullptr;
         q->schedule_all();
